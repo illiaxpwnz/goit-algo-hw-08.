e932db3f5e7feee4833275609cb3a687a289b3cf
Illia <79321689+illiaxpwnz@users.noreply.github.com>
2024-05-22 10:52:23 +0300

    Update heapq_sort.py

--- heapq_sort.py
+++ heapq_sort.py
@@ -7,9 +7,14 @@ def heapsort(iterable):
         heapq.heappush(h, value)
     while h:
         result.append(heapq.heappop(h))
-
     return result
 
-unsorted_list = [8, 4, 6, 12, 15, 2, 10]
-sorted_list = heapsort(unsorted_list)
-print("Відсортований список:", sorted_list)
+def heapsort_nested(nested_iterable):
+    sorted_sublists = []
+    for sublist in nested_iterable:
+        sorted_sublists.append(heapsort(sublist))
+    return sorted_sublists
+
+unsorted_lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
+sorted_lists = heapsort_nested(unsorted_lists)
+print("Відсортовані списки:", sorted_lists)
